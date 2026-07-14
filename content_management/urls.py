9738a9fe6a68067f8e575acc70a844ae3dbf3e44
BelieveMko <mukobelieve@gmail.com>
2026-07-14 13:02:45 +0200

    feat: Implement content read functionality

--- content_management/urls.py
+++ content_management/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, ContentListView, ContentRetrieveView
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
+    path('content/', ContentListView.as_view()),
+    path('content/<slug:slug>/', ContentRetrieveView.as_view())
 ]
--- content_management/views.py
+++ content_management/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import RegisterSerializer
+from .models import Content, Category, ContentView
+from .serializers import (RegisterSerializer,
+                          ContentListSerializer,
+                          ContentDetailSerializer,
+                          ContentCreateSerializer,
+                          CategorySerializer,
+                          TagSerializer
+                          )
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
+class ContentListView(generics.ListAPIView):
+    queryset = Content.objects.filter(is_published=True)
+    serializer_class = ContentListSerializer
+
+
+class ContentRetrieveView(generics.RetrieveAPIView):
+    queryset = Content.objects.filter(is_published=True)
+    serializer_class = ContentDetailSerializer
+    lookup_field = "slug"
