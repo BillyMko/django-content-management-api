85d48c17fbd8206050f94fbe17c6fc4ae54d791a
BelieveMko <mukobelieve@gmail.com>
2026-07-15 20:54:54 +0200

    feat: Add content create and list endpoint with permissions

--- content_management/urls.py
+++ content_management/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import RegisterView, ContentListView, ContentRetrieveView
+from .views import RegisterView, ContentRetrieveView, ContentListCreateView
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
-    path('content/', ContentListView.as_view()),
-    path('content/<slug:slug>/', ContentRetrieveView.as_view())
+    path('content/<slug:slug>/', ContentRetrieveView.as_view(), name="content-retrieve"),
+    path('content/', ContentListCreateView.as_view(), name= "content-list-create")
 ]
--- content_management/views.py
+++ content_management/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Content, Category, ContentView
 from .serializers import (RegisterSerializer,
                           ContentListSerializer,
@@ -21,3 +22,22 @@ class ContentRetrieveView(generics.RetrieveAPIView):
     queryset = Content.objects.filter(is_published=True)
     serializer_class = ContentDetailSerializer
     lookup_field = "slug"
+
+class ContentListCreateView(generics.ListCreateAPIView):
+    queryset = Content.objects.all()
+
+    def get_serializer_class(self):
+
+        if self.request.method == "POST":
+            return ContentCreateSerializer
+        
+        return ContentListSerializer
+    
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsAuthenticated()]
+        
+        return [AllowAny()]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
