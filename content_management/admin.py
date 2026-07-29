a7db0154837bb0eabd8fe94f2734ee1d6307feca
BelieveMko <mukobelieve@gmail.com>
2026-07-29 21:29:14 +0200

    feat: customize Content admin interface

--- content_management/admin.py
+++ content_management/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Content
 
-# Register your models here.
+# admin.site.register(Content)
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "category",
+        "difficulty",
+        "status",
+        "is_published",
+        "author",
+        "view_count",
+        "created_at",
+    ]
+
+    list_filter = [
+        "difficulty",
+        "status",
+        "is_published",
+        "category",
+    ]
+
+    search_fields = [
+        "title",
+        "body",
+    ]
+
+    ordering = [
+        "-created_at"
+    ]
+
+    filter_horizontal = [
+        "tags"
+    ]
+
+    readonly_fields = [
+        "created_at",
+        "updated_at"
+    ]
+
+    @admin.display(description="Views")
+    def view_count(self, obj):
+        return obj.views.count()
+
+
