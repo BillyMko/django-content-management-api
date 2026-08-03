711ef804bbe6228ef399533c8e4f71e21744bb2d
BelieveMko <mukobelieve@gmail.com>
2026-08-03 22:25:48 +0200

    fix: configure ALLOWED_HOSTS to use render environmental variables

--- content_management_api/settings.py
+++ content_management_api/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY =  config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda value: value.split(","))
 
 # Application definition
 
