082aff7c3ade4cab40d16d0f1987480141fb6f61
BelieveMko <mukobelieve@gmail.com>
2026-08-03 23:35:28 +0200

    fix: install rest_framework

--- content_management_api/settings.py
+++ content_management_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'content_management',
+    'rest_framework',
     'django_filters',
     'drf_spectacular',
 ]
